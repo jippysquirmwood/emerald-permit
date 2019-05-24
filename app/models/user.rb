ab3a8afa71895bdf47dd87390422fa917b55415d
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-24 17:13:34 +0100

    added avatar and seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :authored_permits, class_name: "Permit", foreign_key: "author_id"
   has_many :approved_permits, class_name: "Permit", foreign_key: "approver_id"
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
