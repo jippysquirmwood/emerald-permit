718f39688773e928abe59392eea6890a2d765217
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-24 16:37:13 +0100

    navbar mk1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,4 +25,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     permits_path
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
