e9f220de4e05a64ade0ae30b2a6506da6ff012c7
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-05-28 13:15:55 +0100

    100 permits created

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,4 +51,65 @@ users_attributes = [
 
 User.create!(users_attributes)
 puts "Users created"
+
+statuses = ["draft", "rejected", "pending approval", "approved", "expired"]
+permit_types = ["permit to dig", "permit to drill", "permit to penetrate", "permit to work at height", "permit to load", "confined space permit"]
+levels = ["B02 - Basement 2", "B01 - Basement 1", "G01 - Ground", "L01 - Level 1", "L02 - Level 2", "L03 - Level 3", "R01 - Roof"]
+locations = ["west corridor", "toilet block", "staff accommodation room", "lobby", "drainage culvert"]
+project_id = "P257-COT"
+permit_numbers = (10..90).step(10).to_a
+booleans = [true, false]
+cat_models = ["C.A.T4", "C.A.T4+", "eC.A.T4", "eC.A.T4+", "gC.A.T4", "gC.A.T4+", nil]
+genny_models = ["Genny4", nil]
+titles = ["Trench for culvert 12B", "Drill out column footings", "Northern Abutment batter trim", "Southern Abutment batter trim", "Trial Dig", "SIGI sitewide", "Topsoil Strip", "Cut to fill from Borrow Pit C", "Rock breaking in Cut A"]
+
+puts 'Creating permits...'
+100.times do
+  Permit.create!(
+    {
+      status: statuses.sample,
+      start_date: Time.now() + rand(10).day,
+      end_date: Time.now() + (10 + rand(5)).day,
+      permit_type: permit_types.sample,
+      level: rand < 0.5 ? levels[2] : levels.sample,
+      location: locations.sample,
+      project_id: project_id,
+      permit_number: project_id + "-CIV-PERM-00" + permit_numbers.sample.to_s + rand(9).to_s,
+      method_statement: project_id + "-WPP-CS-000462/493",
+      xwidth: (0..10000).step(25).to_a.sample,
+      xlength: (0.5..50.0).step(0.1).to_a.sample,
+      xdepth: (0..10000).step(25).to_a.sample,
+      gas: booleans.sample,
+      fibre_optic: booleans.sample,
+      water: booleans.sample,
+      unknown: false,
+      hv_electricity: booleans.sample,
+      bt: booleans.sample,
+      traffic_signal: booleans.sample,
+      signal_cable: booleans.sample,
+      lv_electricity: booleans.sample,
+      drains: booleans.sample,
+      street_lighting: booleans.sample,
+      no_services: false,
+      hand_digging: booleans.sample,
+      confirm_work_package_plan: booleans.sample,
+      confirm_machine_operator: booleans.sample,
+      services_protected: booleans.sample,
+      exlusion_zone: booleans.sample,
+      buried_cables_identified: booleans.sample,
+      drawing_updated: booleans.sample,
+      physical_barrier_available: booleans.sample,
+      excavation_undermining_foundations: booleans.sample,
+      cat_scan_model: cat_models.sample,
+      genny_model: genny_models.sample,
+      calibration_expires: Time.now() + 180.day,
+      cat_scanner_serial: "AR003BYARDFD-D93485839394",
+      genny_serial: "FD-D93AR003BYARD485839394",
+      author_id: User.where(author: true).sample.id,
+      approver_id: User.where(approver: true).sample.id,
+      title: titles.sample
+    }
+  )
+end
+puts "Permits created"
 puts 'Finished!'
