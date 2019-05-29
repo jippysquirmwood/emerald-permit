6d7ac6d7a6b95c4fc9d71e18197759851731b1e4
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-05-29 17:54:24 +0100

    added .strftime methods

--- app/models/permit.rb
+++ app/models/permit.rb
@@ -28,4 +28,35 @@ class Permit < ApplicationRecord
   def author_email
     author.email
   end
+
+  def permit_type_img
+    if permit_type == "permit to dig"
+      img_url = "dig.png"
+    elsif permit_type == "permit to drill"
+      img_url = "drill.png"
+    elsif permit_type == "permit to work at height"
+      img_url = "work_at_height.png"
+    elsif permit_type == "permit to load"
+      img_url = "load.png"
+    elsif permit_type == "confined space permit"
+      img_url = "confined_space.jpg"
+    elsif permit_type == "hot work permit"
+      img_url = "hot_works.png"
+    else
+      img_url = "logo-emerald-lrg-bright.png"
+    end
+    img_url
+  end
+
+  def short_date(date)
+     date.strftime("%e %b %y")
+  end
+
+  def date_time(date)
+    date.strftime("%-d-%m-%y %H:%M")
+  end
+
+  def time(date)
+    date.strftime("%H:%M")
+  end
 end
