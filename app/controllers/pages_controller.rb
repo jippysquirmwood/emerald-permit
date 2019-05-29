0161a4662ff0d92b8e4e0ddd6a7557ec4aeec6f4
Zac Hobbs <zac.hobbs@gmail.com>
2019-05-29 11:27:03 +0100

    index moved to dashboard and routes updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :dashboard]
 
   def home
   end
 
   def dashboard
+    @permits = policy_scope(Permit)
+    if params[:status].present?
+      @all_permits = Permit.where(status: params[:status])
+    else
+      @all_permits = Permit.all
+    end
     @pending_permits = Permit.where(status: "pending approval").where(approver_id: current_user.id).order(start_date: :asc)
     @approved_permits = Permit.where(status: "approved").where(approver_id: current_user.id).order(start_date: :asc)
     @rejected_permits = Permit.where(status: "rejected").where(approver_id: current_user.id).order(start_date: :asc)
