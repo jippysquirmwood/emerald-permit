051f33c46d24d29b568d58d3b527fbd8ef04a1bb
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-06-06 16:40:32 +0100

    demo seed date set

--- db/seeds.rb
+++ db/seeds.rb
@@ -278,7 +278,7 @@ end
 puts "Random Permits created"
 
 puts "Creating Jono's permits"
-presentation_time = Date.new(2019, 6, 6) + 16.hours
+presentation_time = Date.new(2019, 6, 7) + 18.hours
 # presentation_time = Time.now.to_datetime + 1.hours
 Permit.create!(
   {
